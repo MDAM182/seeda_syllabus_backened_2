f5f71b0c0fe1442866adbe8c38ef54af41cbab57
MDAM182 <mdam2345@gmail.com>
2021-04-01 23:57:03 -0400

    syllabus controller index and create routes

--- app/controllers/api/v1/syllabuses_controller.rb
+++ app/controllers/api/v1/syllabuses_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::SyllabusesController < ApplicationController
+  def index
+    @ayllabuses = Syllabus.all
+    render json: @syllabuses
+  end
+
+  def create
+    syllabus = Syllabus.new(syllabus_params)
+    if syllabus.save
+        render json: syllabus, status: :accepted
+    else
+      render json: { errors: syllabus.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def syllabus_params
+    require(:syllabus).permit(:title, :description, :image_url, :category_id)
+    params.permit()
+  end
+
 end
